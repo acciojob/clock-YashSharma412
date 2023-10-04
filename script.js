fdf1ae8d012b286ffe6a5963a2ccf54be36077e6
Yash Sharma <139811734+YashSharma412@users.noreply.github.com>
2023-10-04 18:59:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function ClockDisplayUpdate() {
+	const now = new Date();
+	const dateString = now.toLocaleString();
+	document.getElementById('timer').innerText = dateString;
+	
+}
+ClockDisplayUpdate();
+setInterval(ClockDisplayUpdate, 1000);
